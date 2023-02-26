f62c57e0bc88c87169b1fbcea9e1bb03eb0ec4c4
Rampal00 <104526427+Rampal00@users.noreply.github.com>
2023-02-27 01:03:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -345,8 +345,8 @@ async def account_login(bot: Client, m: Message):
             try:
                 Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`"
                 prog = await m.reply_text(Show)
-                cc = f'**Name »** {name1} {res}.mp4\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n **Downloaded By :ＲＯＬＥＸ**'
-                cc1 = f'**Name »** ** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n **Downloaded By :ＲＯＬＥＸ**'
+                cc = f'**Name »** {name1} {res}.mp4\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Downloaded By :ＲＯＬＥＸ**'
+                cc1 = f'**Name »** ** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Downloaded By :ＲＯＬＥＸ**'
                 #                         await prog.delete (True)
                 #                 if cmd == "pdf" or "drive" in url:
                 #                     try:
@@ -378,7 +378,7 @@ async def account_login(bot: Client, m: Message):
                         await m.reply_document(
                             ka,
                             caption=
-                            f'**Name »** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}'
+                            f'**Name »** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Downloaded By :ＲＯＬＥＸ**'
                         )
                         count += 1
                         # time.sleep(1)
