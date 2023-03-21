edca164b0ba44dbc656558eee267b34a28cc6d3a
Rampal00 <104526427+Rampal00@users.noreply.github.com>
2023-03-21 19:25:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -626,7 +626,7 @@ async def account_login(bot: Client, m: Message):
 
                 response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
                 # print(response.json())
-                a = response.json()[     ]           # print(a)
+                a = response.json()['url']           # print(a)
 
 
                 headers1 = {
