bf45e944838276a0018890ad5d181dc5afbb85bb
Rampal00 <104526427+Rampal00@users.noreply.github.com>
2023-03-27 15:58:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -335,8 +335,8 @@ async def account_login(bot: Client, m: Message):
             try:
                 Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`"
                 prog = await m.reply_text(Show)
-                cc = f'**Vid_Id : ** {str(count).zfill(3)}\n\n**Title : ** {name1} @REXODAS {res}.mkv\n\n**Batch : ** {raw_text0}\n\n**Downloaded By :** {raw_text8}\n\n**Join Backup :-** @REXODAS2023'
-                cc1 =f'**Pdf_Id : ** {str(count).zfill(3)}\n\n**Title : ** {name1} @REXODAS.pdf\n\n**Batch : ** {raw_text0}\n\n**Downloaded By :** {raw_text8}\n\n**Join Backup :- ** @REXODAS2023'
+                cc = f'**Vid_Id : ** {str(count).zfill(3)}\n\n**Title : ** {name1} {res}.mkv\n\n**Batch : ** {raw_text0}\n\n**Downloaded By :** {raw_text8}'
+                cc1 =f'**Pdf_Id : ** {str(count).zfill(3)}\n\n**Title : ** {name1}.pdf\n\n**Batch : ** {raw_text0}\n\n**Downloaded By :** {raw_text8}'
                 #                         await prog.delete (True)
                 #                 if cmd == "pdf" or "drive" in url:
                 #                     try:
@@ -368,7 +368,7 @@ async def account_login(bot: Client, m: Message):
                         await m.reply_document(
                             ka,
                             caption =
-                            f'**Pdf_Id : ** {str(count).zfill(3)}\n\n**Title : ** {name1} @Rexo_Talk.pdf\n\n**Batch : ** {raw_text0}\n\n**Downloaded By :** {raw_text8}\n\n**Join Backup :- @REXODAS2023'
+                            f'**Pdf_Id : ** {str(count).zfill(3)}\n\n**Title : ** {name1}.pdf\n\n**Batch : ** {raw_text0}\n\n**Downloaded By :** {raw_text8}'
                         )
                         count += 1
                         # time.sleep(1)
