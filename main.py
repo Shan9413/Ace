fdc1d414c151fb264b3a706be06b0b600e92fe8e
Rampal00 <104526427+Rampal00@users.noreply.github.com>
2023-04-06 00:25:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -520,7 +520,7 @@ async def account_login(bot: Client, m: Message):
             name = f'{str(count).zfill(3)}) {name1}'
             Show = f"**Downloading:-**\n\n**Name :-** `{name}`\n\n**Url :-** `{url1}`"
             prog = await m.reply_text(Show)
-            cc = f'**Title »** {name1}.mkv\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- Group Admin'
+            cc = f'**Title »** {name1}.mkv\n\n**Caption »** {raw_text0}\n\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- Group Admin'
             if "pdf" in url:
                 cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
             else:
