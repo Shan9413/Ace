abe96916d49c0583cccc3293cf022c3bed6ff611
Rampal00 <104526427+Rampal00@users.noreply.github.com>
2023-04-06 00:42:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -524,7 +524,9 @@ async def account_login(bot: Client, m: Message):
             if "pdf" in url:
                 cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
             else:
-                cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --remux-video mkv "{url1}"'
+                cmd = f'yt-dlp --no-keep-video --no-check-certificate --remux-video mkv "{url1}" -o "{name}.%(ext)s"'
+            else:
+                cmd = f'yt-dlp -f "{ytf}" --no-check-certificate --remux-video mkv "{url1}" -o "{name}.%(ext)s"'
             try:
                 download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
                 os.system(download_cmd)
