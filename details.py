b3d7d47bcef57b5ae216e821d3cabf2d40882f87
Shankar <120117175+Saanusalvi1@users.noreply.github.com>
2023-11-28 19:31:16 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 20088962
 api_hash = "257f47d347157555890a64b12bc0134f"
-bot_token = "6134747255:AAFnOrKgIGlQ4lORCmonc6sE_aYm8ASmsJY"
+bot_token = "6389756670:AAHXFERgeoJ_VjtvKx38M1ivGnsvoRDsLuM"
 
