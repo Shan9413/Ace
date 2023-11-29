2bc5ceb4d2cdacbcdbc595b9db5749310f615e09
Shankar <120117175+Saanusalvi1@users.noreply.github.com>
2023-11-30 01:30:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,12 +41,10 @@ import io
 bot = Client("bot",
              bot_token=6654605912:AAFy2Js6kiwUq1a18pt3O5zPReCAHEw9XPE,
              api_id=20088962,
-             api_hash=257f47d347157555890a64b12bc0134f
-auth_users = [
-    int(chat) for chat in os.environ.get("6169016546").split(",") if chat != '']
+             api_hash=257f47d347157555890a64b12bc0134f, 
+auth_users =6169016546,
 sudo_users = auth_users
-sudo_groups = [
-    int(chat) for chat in os.environ.get("-1004008382140").split(",") if chat != '']
+sudo_groups =-1004008382140, 
     
     
 @bot.on_message(filters.command(["pyro"]))
