75d197145b489e59dcbd0026c48b2071d1379e14
Shan9413 <159189394+Shan9413@users.noreply.github.com>
2024-02-10 09:08:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,12 +39,9 @@ import io
 
 
 bot = Client("bot",
-             bot_token=6746876285:AAGnUDNwDvWu4RfbkSSiQe8gt-2lU-9p-Bk,
-             api_id=20088962,
-             api_hash=257f47d347157555890a64b12bc0134f, 
-auth_users =6169016546,
-sudo_users = auth_users
-sudo_groups =-1004008382140, 
+             bot_token= "6923349541:AAFc7_MEXoKYHajAKFTtMo4QzUb8KO7J82E",
+             api_id= 20088962,
+             api_hash= "257f47d347157555890a64b12bc0134f")
     
     
 @bot.on_message(filters.command(["pyro"]))
